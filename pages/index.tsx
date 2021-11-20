a02bd5164066666dd019c90bd6fccd0b1e99ccaf
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-20 23:46:41 +0900

    とりあえずデータの取得に成功。ネットの情報通りに行かなかった。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import * as contentful from 'contentful';
 
 const index = () => {
   return <div>Hello</div>;
 };
 
 export default index;
+
+const config = {
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+};
+
+(async () => {
+  const client = contentful.createClient(config);
+
+  const entryId = '3EBAmzacsEo0OJI9MnZwkd';
+  const entry = await client.getEntry(entryId);
+  console.log(entry); // 対象のEntry内容を取得できる
+})();

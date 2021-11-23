812f4d91f0128dab76e998f2c594f9786b5e2cdb
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-24 08:29:27 +0900

    画像表示ができた！Import文とかミスってたw

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,21 @@
 import { fetchEntries } from '../../lib/contentful';
 import { getAllPostIds, getPostData } from '../../lib/posts';
-import { Image } from 'next';
+import Image from 'next/image';
 
 export default function Post({ postData }) {
   // console.log(postData);
   const { title, image, body } = postData.fields;
-  console.log(image);
+  console.log(image.fields.file.url);
   return (
     <>
       <h1>{title}</h1>
-      {/* <Image
+      <Image
         src={`http:${image.fields.file.url}`}
         alt={image.fields.description}
-      /> */}
+        width="300"
+        height="400"
+        objectFit="contain"
+      />
     </>
   );
 }
